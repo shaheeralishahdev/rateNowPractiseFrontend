ec983475a974e523434cd1e69349bb6aebb01ccd
M Hamza Farooqi <muhamzafarooqi9211@gmail.com>
2026-02-03 13:01:24 +0500

    implement redirect url

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,19 +1,27 @@
 import { useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate,useSearchParams } from 'react-router-dom';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('test@test.com');
   const [password, setPassword] = useState('123456');
   const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const redirectUrl = searchParams.get("redirectUrl"); // get ?redirectUrl=
+  
 //   const url = 'http://localhost:5000'
 const url = 'https://ratenowpractisebackend-production.up.railway.app'
   const handleLogin = async () => {
     try {
-      await axios.post(`${url}/login`, { email, password }, {
+      await axios.post(`${url}/login`, { email, password,redirectUrl }, {
         withCredentials: true // important to store cookie
       });
-      navigate('/home');
+      if (redirectUrl) {
+        // Redirect to mobile deep link with token
+        // window.location.href = `${redirectUri}?token=${token}`;
+      } else {
+        navigate('/home');
+      }
     } catch(err) {
       alert('Login failed');
     }
